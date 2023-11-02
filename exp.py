2a092395be1f4daf124ff7b072bbfb412b024606
jesusverma <jesus.verma@gmail.com>
2023-11-03 00:15:08 +0530

    added decsion tree model type

--- exp.py
+++ exp.py
@@ -6,10 +6,19 @@ import itertools
 
 # Import datasets, classifiers and performance metrics
 from sklearn import metrics, svm
+
 from utils import  preprocess_data, split_data, train_model, read_digits, split_train_dev_test, predict_and_eval, tune_hparams, get_hyperparameter_combinations
 from joblib import dump, load
-
+import pandas as pd
 ###############################################################################
+
+
+
+#1. Get the dataset
+X,y = read_digits()
+
+# 2. Define common Hyper parameter combinations
+classifier_param_dict = {}
 
 gamma_ranges = [0.001, 0.01, 0.1, 1, 10, 100]
 c_ranges = [0.1,1,2,5,10]
@@ -19,13 +28,15 @@ h_params={}
 h_params['gamma'] = gamma_ranges
 h_params['C'] = c_ranges
 h_params_combinations = get_hyperparameter_combinations(h_params)
+classifier_param_dict['svm'] = h_params_combinations
 
 
-#1. Get the dataset
-X,y = read_digits()
-
-
-
+#  Decision Tree h params
+max_depth_list = [5, 10, 15, 20, 50, 100]
+h_params_tree = {}
+h_params_tree['max_depth'] = max_depth_list
+h_params_trees_combinations = get_hyperparameter_combinations(h_params_tree)
+classifier_param_dict['tree'] = h_params_trees_combinations
 
 
 
@@ -65,31 +76,43 @@ X,y = read_digits()
 
 
 
-test_sizes =  [0.1, 0.2, 0.3, 0.45]
-dev_sizes =  [0.1, 0.2, 0.3, 0.45]
+results = []
+test_sizes =  [0.2]
+dev_sizes  =  [0.2]
 
-for test_size in test_sizes:
-    for dev_size in dev_sizes:
-        train_size = 1- test_size - dev_size
+
+num_runs  = 5
+
+for cur_run_i in range(num_runs):
+    for test_size in test_sizes:
+        for dev_size in dev_sizes:
+            train_size = 1- test_size - dev_size
+            
+            X_train, X_dev, X_test, y_train, y_dev, y_test = split_train_dev_test(X,y, dev_size , test_size)
+
+            X_train = preprocess_data(X_train)
+            X_test = preprocess_data(X_test)
+            X_dev = preprocess_data(X_dev)
+
+            for model_type in classifier_param_dict:
+                model_type_hparams = classifier_param_dict[model_type]
+
+                best_hparams,best_model_path,best_accuracy_so_far  = tune_hparams(X_train, y_train, X_dev, 
+                y_dev, model_type_hparams,model_type)  
+
+                print(best_model_path)
+                best_model_so_far = load(best_model_path) 
+
+                test_accuracy = predict_and_eval(best_model_so_far,X_test,y_test) 
+                train_accuracy = predict_and_eval(best_model_so_far,X_train,y_train) 
+                dev_accuracy = best_accuracy_so_far
+
+                print("{}\ttest_size={:.2f} dev_size={:.2f} train_size={:.2f} train_accuracy={:.2f} dev_accuracy={:.2f} test_accuracy={:.2f}".format(model_type, test_size, dev_size, train_size, train_accuracy, dev_accuracy, test_accuracy))
+                
+                cur_run_results = {'model_type': model_type, 'run_index': cur_run_i, 'train_accuracy' : train_accuracy, 'dev_accuracy': dev_accuracy, 'test_accuracy': test_accuracy}
+                results.append(cur_run_results)            
         
-        X_train, X_dev, X_test, y_train, y_dev, y_test = split_train_dev_test(X,y, dev_size , test_size)
-
-        X_train = preprocess_data(X_train)
-        X_test = preprocess_data(X_test)
-        X_dev = preprocess_data(X_dev)
-
-        best_hparams,best_model_path,best_accuracy_so_far  = tune_hparams(X_train, y_train, X_dev, 
-        y_dev, h_params_combinations)  
-        print(best_model_path)
-        best_model_so_far = load(best_model_path) 
-
-        test_accuracy = predict_and_eval(best_model_so_far,X_test,y_test) 
-        train_accuracy = predict_and_eval(best_model_so_far,X_train,y_train) 
-        dev_accuracy = best_accuracy_so_far
-        print("test_size={:.2f} dev_size={:.2f} train_size={:.2f} train_acc={:.2f} dev_acc={:.2f} test_acc={:.2f}".format(test_size, dev_size, train_size, train_accuracy, dev_accuracy, test_accuracy))
-        
-        
-
+print(pd.DataFrame(results).groupby('model_type').describe().T)
 
 
 # 3. Vary the test_size in [0.1, 0.2, 0.3] and dev_size in [0.1, 0.2, 0.3], and github actions should output something like: 
